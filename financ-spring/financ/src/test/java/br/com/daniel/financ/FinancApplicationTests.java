8e82d4bc972be6e2ccec1b47ebcc55b45d211875
Daniel Pereira Vasconcelos nunes <daniel.pvn@hotmail.com>
2024-08-12 20:04:58 -0300

    angular + spring
    criando as pastas para desenvolvimento

---
+++ financ-spring/financ/src/test/java/br/com/daniel/financ/FinancApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.financ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
